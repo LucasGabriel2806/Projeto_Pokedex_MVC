a366bb509e5ea9fb08ad430f629b95b3fc70d19c
Lucas Gabriel <pintolucas996@gmail.com>
2022-08-30 15:46:05 -0300

    Interface melhorada

--- index.php
+++ index.php
@@ -1,3 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+    <title>Pokédex</title>
+</head>
+<body>
+
+
+    <div id="site">
+        <div id="tela_esquerda">
+            <img src="Img/logo_pokemon.png" width="600" height="200"><br>
+                <p id="Disponiveis">Lista de Pokémons disponiveis:</p>
+            
+        
+            <ul id="poke_lista">
+                <li>(1) Bulbasauro</li>
+                <li>(2) Charmander</li>
+                <li>(3) Charmeleon</li>
+                <li>(4) Charizard</li>
+                <li>(5) Squirtle</li>
+                <li>(6) Pikachu</li>
+                <li>(7) Butterfree</li>
+                <li>(8) Pidgeotto</li>
+                <li>(9) Mew</li>
+                <li>(10) Mewtwo</li>
+            </ul>
+
+        </div>
+        <div id="tela_direita">
+            <img src="Img/pokedex.png" width="1100" height="850">
+            
+        </div>
+    </div>
+
+
+
+    
+</body>
+</html>
+
+
+
 <?php
 
 $uri_parse = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
@@ -83,15 +129,4 @@ switch($uri_parse)
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+?>
